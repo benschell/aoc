bbf8e25a97572cbec09450006bcd2a91dbc86a7f
Benjamin Schell <ben.schell@gmail.com>
2024-12-11 11:43:16 -0500

    2024 day 11 part 2

--- aoc2024/README.md
+++ aoc2024/README.md
@@ -21,7 +21,7 @@
 [![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%85/green)](src/day08)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
 [![Day](https://badgen.net/badge/10/%E2%98%85%E2%98%85/green)](src/day10)
-[![Day](https://badgen.net/badge/11/%E2%98%85%E2%98%86/yellow)](src/day11)
+[![Day](https://badgen.net/badge/11/%E2%98%85%E2%98%85/green)](src/day11)
 ![Day](https://badgen.net/badge/12/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/13/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/14/%E2%98%86%E2%98%86/gray)
@@ -139,9 +139,9 @@ Both parts: 5.151ms
 
 ```
 Day 11
-Time part 1: 1093.903ms
-Time part 2: -
-Both parts: 1093.903ms
+Time part 1: 2.785ms
+Time part 2: 112.204ms
+Both parts: 114.989ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 19/50
-Total time: 31626.722ms
+Total stars: 20/50
+Total time: 30647.808ms
 ```
 
 <!--/RESULTS-->
--- aoc2024/src/day11/index.ts
+++ aoc2024/src/day11/index.ts
@@ -8,43 +8,83 @@ const log = (...str: any[]) => {
   }
 };
 
-const parseInput = (rawInput: string) => rawInput.split(" ").map((num) => parseInt(num));
-const doBlinks = (input: number[], blinks: number) => {
-  log('start:', input);
-  for (let i=0; i < blinks; i++) {
-    for (let j=0; j<input.length; j++) {
-      const num = input[j];
-      const numStr = `${num}`;
-      if (num === 0) {
-        input[j] = 1;
-      } else if (numStr.length % 2 === 0) {
-        const half = numStr.length / 2;
-        const first = parseInt(numStr.substring(0, half));
-        const second = parseInt(numStr.substring(half));
-        input.splice(j, 1, first, second);
-        j += 1; // we added one item, so we need to move forward
-      } else {
-        input[j] = num * 2024;
-      }
-    }
-
-    log(`after ${i+1} blinks:`);
-    log(input);
+const parseInput = (rawInput: string) =>
+  rawInput.split(" ").map((num) => parseInt(num));
+const doBlinksForNum: (
+  num: number,
+  blinks: number,
+  logPadding: string,
+) => number = (num, blinks, lp) => {
+  if (blinks === 0) {
+    return 1;
   }
-}
+  log(`\n${lp}doing ${blinks} blinks for ${num}`);
+  const input = [num];
+  log(`${lp}input:`, input);
+  // for (let i=0; i < blinks; i++) {
+  for (let j = 0; j < input.length; j++) {
+    const num = input[j];
+    const numStr = `${num}`;
+    if (num === 0) {
+      input[j] = 1;
+    } else if (numStr.length % 2 === 0) {
+      const half = numStr.length / 2;
+      const first = parseInt(numStr.substring(0, half));
+      const second = parseInt(numStr.substring(half));
+      input.splice(j, 1, first, second);
+      j += 1; // we added one item, so we need to move forward
+    } else {
+      input[j] = num * 2024;
+    }
+  }
+
+  log(`${lp}with ${blinks - 1} blinks left:`);
+  log(`${lp}`, input);
+  // }
+  const out = doBlinks(input, blinks - 1, `${lp}\t`);
+  log(`${lp}returning:`, out, input);
+  return out;
+};
+const priors = new Map<string, number>();
+const key = (num: number, blinks: number) => `${num}-${blinks}`;
+const doBlinks = (
+  input: number[],
+  blinks: number,
+  logPadding: string,
+  shouldLog?: boolean,
+) => {
+  return input.reduce((score, num) => {
+    const k = key(num, blinks);
+    const found = priors.has(k);
+    // console.log("lookup:", k, priors.has(k), priors.get(k));
+    const count = found
+      ? priors.get(k)!
+      : doBlinksForNum(num, blinks, logPadding);
+    if (!found) {
+      priors.set(k, count);
+    }
+    // if (shouldLog) {
+    //   console.log(`${logPadding}did blinks for`, num, count);
+    // }
+    return score + count;
+  }, 0);
+};
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
   const blinks = 25;
-  doBlinks(input, blinks);
-
-  return input.length;
+  log("\n\nstart:", input);
+  return doBlinks(input, blinks, "");
 };
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
-
-  return;
+  // 40: 22.4 sec
+  // 41: 29.1 sec : +  7
+  // 42: 43.0 sec : + 21
+  // 43: 66.4 sec : + 44
+  const blinks = 75;
+  return doBlinks(input, blinks, "", true);
 };
 
 run({
