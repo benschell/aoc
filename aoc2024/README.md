02ff5e396175856051ade82bd0908bf3b305ea78
Benjamin Schell <ben.schell@gmail.com>
2024-12-12 16:01:50 -0500

    2024 day 12 part 2

--- aoc2024/README.md
+++ aoc2024/README.md
@@ -22,7 +22,7 @@
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
 [![Day](https://badgen.net/badge/10/%E2%98%85%E2%98%85/green)](src/day10)
 [![Day](https://badgen.net/badge/11/%E2%98%85%E2%98%85/green)](src/day11)
-[![Day](https://badgen.net/badge/12/%E2%98%85%E2%98%86/yellow)](src/day12)
+[![Day](https://badgen.net/badge/12/%E2%98%85%E2%98%85/green)](src/day12)
 ![Day](https://badgen.net/badge/13/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/14/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/15/%E2%98%86%E2%98%86/gray)
@@ -146,9 +146,9 @@ Both parts: 114.989ms
 
 ```
 Day 12
-Time part 1: 26.039ms
-Time part 2: -
-Both parts: 26.039ms
+Time part 1: 30.741ms
+Time part 2: 30.496ms
+Both parts: 61.237ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 21/50
-Total time: 30673.847ms
+Total stars: 22/50
+Total time: 30709.045ms
 ```
 
 <!--/RESULTS-->
--- aoc2024/src/day12/index.ts
+++ aoc2024/src/day12/index.ts
@@ -12,8 +12,19 @@ const log = (...str: any[]) => {
 const parseInput = (rawInput: string) =>
   rawInput.split("\n").map((row) => row.split(""));
 
-const part1 = (rawInput: string) => {
-  const map = parseInput(rawInput);
+const solve = (map: string[][], partTwo?: boolean): number => {
+  const nodeMap: {
+    x: number;
+    y: number;
+    leftFence?: boolean;
+    leftFenceCounted?: boolean;
+    rightFence?: boolean;
+    rightFenceCounted?: boolean;
+    upFence?: boolean;
+    upFenceCounted?: boolean;
+    downFence?: boolean;
+    downFenceCounted?: boolean;
+  }[][] = [];
   printMap(map, log);
   const visited = new Set<string>();
   let score = 0;
@@ -33,84 +44,219 @@ const part1 = (rawInput: string) => {
           // From this starting place...
           const node = toVisit.pop()!;
           const { x, y } = coordFromKey(node);
+          nodeMap[y] ??= [];
+          nodeMap[x] ??= [];
+          nodeMap[y][x] = { x, y };
           let hasFound = false;
           // ... Look left, right, up, down
           let k;
           k = key(x - 1, y);
-          if (x > 0 && !visited.has(k) && map[y][x - 1] === curr) {
+          if (x > 0 && map[y][x - 1] === curr) {
             // Left is part of our region
-            visited.add(k);
-            region.add(k);
-            toVisit.push(k);
-            hasFound = true;
+            nodeMap[y][x].leftFence = false;
+            if (!visited.has(k)) {
+              visited.add(k);
+              region.add(k);
+              toVisit.push(k);
+              hasFound = true;
+            }
           }
           k = key(x + 1, y);
-          if (x < row.length - 1 && !visited.has(k) && map[y][x + 1] === curr) {
+          if (x < row.length - 1 && map[y][x + 1] === curr) {
             // Right is part of our region
-            visited.add(k);
-            region.add(k);
-            toVisit.push(k);
-            hasFound = true;
+            nodeMap[y][x].rightFence = false;
+            if (!visited.has(k)) {
+              visited.add(k);
+              region.add(k);
+              toVisit.push(k);
+              hasFound = true;
+            }
           }
           k = key(x, y - 1);
-          if (y > 0 && !visited.has(k) && map[y - 1][x] === curr) {
+          if (y > 0 && map[y - 1][x] === curr) {
             // Up is part of our region
-            visited.add(k);
-            region.add(k);
-            toVisit.push(k);
-            hasFound = true;
+            nodeMap[y][x].upFence = false;
+            if (!visited.has(k)) {
+              visited.add(k);
+              region.add(k);
+              toVisit.push(k);
+              hasFound = true;
+            }
           }
           k = key(x, y + 1);
-          if (y < map.length - 1 && !visited.has(k) && map[y + 1][x] === curr) {
+          if (y < map.length - 1 && map[y + 1][x] === curr) {
             // Down is part of our region
-            visited.add(k);
-            region.add(k);
-            toVisit.push(k);
-            hasFound = true;
+            nodeMap[y][x].downFence = false;
+            if (!visited.has(k)) {
+              visited.add(k);
+              region.add(k);
+              toVisit.push(k);
+              hasFound = true;
+            }
           }
-          // if (!hasFound) {
-          //   // We've found everything for this region
-          //   break;
-          // }
-          // if (size++ > 10) {
-          //   break;
-          // }
+
+          nodeMap[y][x].leftFence ??= true;
+          nodeMap[y][x].rightFence ??= true;
+          nodeMap[y][x].upFence ??= true;
+          nodeMap[y][x].downFence ??= true;
         }
         log("Found region:", curr, region);
         // Score this region
         const area = region.size;
         const perimeter = [...region].reduce((score, location) => {
+          log(`examining: ${location}, starting score: ${score}`);
           const { x, y } = coordFromKey(location);
-          const curr = map[y][x];
-          let k;
-          k = key(x - 1, y);
-          if (x <= 0 || map[y][x - 1] !== curr) {
-            // Left is a fence
-            // log(`Left of ${location} is a fence!`);
-            score += 1;
+          const node = nodeMap[y][x];
+          if (!partTwo) {
+            if (node.leftFence) {
+              score += 1;
+            }
+            if (node.rightFence) {
+              score += 1;
+            }
+            if (node.upFence) {
+              score += 1;
+            }
+            if (node.downFence) {
+              score += 1;
+            }
+          } else {
+            // Consider an upFence.
+            if (node.upFence && !node.upFenceCounted) {
+              // Count the upFence
+              score += 1;
+              node.upFenceCounted = true;
+              log(
+                `found upFence\t starting at ${x},${y} and now looking left/right`,
+              );
+              // look to the right and mark upFenceCounted as necessary
+              let n, nx;
+              n = node;
+              nx = x;
+              while (true) {
+                n = nodeMap[y][++nx];
+                if (n && n.upFence) {
+                  n.upFenceCounted = true;
+                  log(`\t up fence counted! ${nx},${y}`);
+                } else {
+                  break;
+                }
+              }
+              n = node;
+              nx = x;
+              while (true) {
+                n = nodeMap[y][--nx];
+                if (n && n.upFence) {
+                  n.upFenceCounted = true;
+                  log(`\t up fence counted! ${nx},${y}`);
+                } else {
+                  break;
+                }
+              }
+            }
+            // Consider a rightFence.
+            if (node.rightFence && !node.rightFenceCounted) {
+              // Count the rightFence
+              score += 1;
+              node.rightFenceCounted = true;
+              log(
+                `found rightFence starting at ${x},${y} and now looking up/down`,
+              );
+              let n, ny;
+              n = node;
+              ny = y;
+              while (ny + 1 < nodeMap.length) {
+                n = nodeMap[++ny][x];
+                if (n && n.rightFence) {
+                  n.rightFenceCounted = true;
+                  log(`\t right fence counted! ${x},${ny}`);
+                } else {
+                  break;
+                }
+              }
+              n = node;
+              ny = y;
+              while (ny - 1 >= 0) {
+                n = nodeMap[--ny][x];
+                if (n && n.rightFence) {
+                  n.rightFenceCounted = true;
+                  log(`\t right fence counted! ${x},${ny}`);
+                } else {
+                  break;
+                }
+              }
+            }
+            // Consider an downFence.
+            if (node.downFence && !node.downFenceCounted) {
+              // Count the downFence
+              score += 1;
+              node.downFenceCounted = true;
+              log(
+                `found downFence\t starting at ${x},${y} and now looking left/right`,
+              );
+              // look to the right and mark downFenceCounted as necessary
+              let n, nx;
+              n = node;
+              nx = x;
+              while (true) {
+                n = nodeMap[y][++nx];
+                if (n && n.downFence) {
+                  n.downFenceCounted = true;
+                  log(`\t down fence counted! ${nx},${y}`);
+                } else {
+                  break;
+                }
+              }
+              n = node;
+              nx = x;
+              while (true) {
+                n = nodeMap[y][--nx];
+                if (n && n.downFence) {
+                  n.downFenceCounted = true;
+                  log(`\t down fence counted! ${nx},${y}`);
+                } else {
+                  break;
+                }
+              }
+            }
+            // Consider a leftFence.
+            if (node.leftFence && !node.leftFenceCounted) {
+              // Count the leftFence
+              score += 1;
+              node.leftFenceCounted = true;
+              log(
+                `found leftFence\t starting at ${x},${y} and now looking up/down`,
+              );
+              let n, ny;
+              n = node;
+              ny = y;
+              while (ny + 1 < nodeMap.length) {
+                n = nodeMap[++ny][x];
+                if (n && n.leftFence) {
+                  n.leftFenceCounted = true;
+                  log(`\t left fence counted! ${x},${ny}`);
+                } else {
+                  break;
+                }
+              }
+              n = node;
+              ny = y;
+              while (ny - 1 >= 0) {
+                n = nodeMap[--ny][x];
+                if (n && n.leftFence) {
+                  n.leftFenceCounted = true;
+                  log(`\t left fence counted! ${x},${ny}`);
+                } else {
+                  break;
+                }
+              }
+            }
           }
-          k = key(x + 1, y);
-          if (x >= row.length - 1 || map[y][x + 1] !== curr) {
-            // Right is a fence
-            // log(`Right of ${location} is a fence!`);
-            score += 1;
-          }
-          k = key(x, y - 1);
-          if (y <= 0 || map[y - 1][x] !== curr) {
-            // Up is a fence
-            // log(`Up of ${location} is a fence!`);
-            score += 1;
-          }
-          k = key(x, y + 1);
-          if (y >= map.length - 1 || map[y + 1][x] !== curr) {
-            // Down is a fence
-            // log(`Down of ${location} is a fence!`);
-            score += 1;
-          }
+          log(`returning score: ${score}`);
           return score;
         }, 0);
         log(`Area: ${area}, Perimeter: ${perimeter}`);
-        score += (area * perimeter);
+        score += area * perimeter;
       }
     }
   }
@@ -118,10 +264,12 @@ const part1 = (rawInput: string) => {
   return score;
 };
 
-const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+const part1 = (rawInput: string) => {
+  return solve(parseInput(rawInput));
+};
 
-  return;
+const part2 = (rawInput: string) => {
+  return solve(parseInput(rawInput), true);
 };
 
 run({
@@ -160,10 +308,51 @@ MMMISSJEEE`,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: ,
-      // },
+      {
+        input: `AAAA
+BBCD
+BBCC
+EEEC`,
+        expected: 80,
+      },
+      {
+        input: `OOOOO
+OXOXO
+OOOOO
+OXOXO
+OOOOO`,
+        expected: 436,
+      },
+      {
+        input: `EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE`,
+        expected: 236,
+      },
+      {
+        input: `AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA`,
+        expected: 368,
+      },
+      {
+        input: `RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE`,
+        expected: 1206,
+      },
     ],
     solution: part2,
   },
